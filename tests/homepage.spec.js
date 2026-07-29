206a5368c68e559001aec2a734b9b05cf23aec76
Kollegala2022 <asshwinim06@gmail.com>
2026-07-29 11:55:58 +0530

    Add playwright test

---
+++ tests/homepage.spec.js
@@ -0,0 +1,37 @@
+const { test, expect } = require('@playwright/test');
+
+test('Book Flats website loads successfully', async ({ page }) => {
+
+    await page.goto('http://localhost:8000/Book%20Flats/index.html');
+
+    await expect(page).toHaveTitle(/Book Flats/i);
+
+    await expect(page.locator('html')).toBeVisible();
+
+    await expect(page.locator('body')).toBeVisible();
+
+});
+
+test('CSS is loaded', async ({ page }) => {
+
+    await page.goto('http://localhost:8000/Book%20Flats/index.html');
+
+    const stylesheets = await page.locator('link[rel="stylesheet"]').count();
+
+    expect(stylesheets).toBeGreaterThan(0);
+
+});
+
+test('Images load correctly', async ({ page }) => {
+
+    await page.goto('http://localhost:8000/Book%20Flats/index.html');
+
+    const images = page.locator('img');
+
+    const count = await images.count();
+
+    for (let i = 0; i < count; i++) {
+        await expect(images.nth(i)).toHaveJSProperty('complete', true);
+    }
+
+});
